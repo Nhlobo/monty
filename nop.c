d7480c14e1338a10509c3b54497f81da7caadf5f
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2023-12-13 10:44:39 +0200

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+#include <stdio.h>
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;  /* Unused parameter */
+    (void)line_number;  /* Unused parameter */
+    /* Does nothing */
+}
